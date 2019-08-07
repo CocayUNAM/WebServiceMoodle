39922f2758d1ed76a8971aa993bffa9570c9bf9d
Héctor Santaella Marín <hectorsama@MacBook-Air-de-Hector.local>
2019-08-06 23:35:33 -0500

    Se agrega las configuraciones para que lea el archivo config.pho

--- WB/config/Database.php
+++ WB/config/Database.php
@@ -1,20 +1,22 @@
 <?php
-    
   class Database {
-
+  
+    
+    
     // DB Params
-    private $host = '127.0.0.1';
-    private $db_name = 'moodle35';
+    //private $host = 'localhost';
+    //private $db_name = 'moodle35';
     private $port='8889';
-    private $username = 'root';
-    private $password = 'root';
+    //private $username = 'root';
+    //private $password = 'root';
     private $conn;
 
     // DB Connect
     public function connect() {
+      require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
       $this->conn = null;
       try {
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';port='. $this->port , $this->username, $this->password);
+        $this->conn = new PDO('mysql:host=' . $CFG->dbhost . ';dbname=' . $CFG->dbname . ';port='. $this->port , $CFG->dbuser, $CFG->dbpass);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
