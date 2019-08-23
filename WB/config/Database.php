88dd8203ebe60d411b75f115a5de1f561017150c
Juan Carlos Hernández <48781621+JCarlosHA@users.noreply.github.com>
2019-08-23 02:15:46 -0500

    Update Database.php

--- WB/config/Database.php
+++ WB/config/Database.php
@@ -1,14 +1,8 @@
 <?php
   class Database {
-  
-    
     
     // DB Params
-    private $host = 'localhost';
-    private $db_name = 'moodle35';
     private $port='8889';
-    private $username = 'root';
-    private $password = 'root';
     private $conn;
 
     // DB Connect
@@ -16,10 +10,15 @@
       require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
       $this->conn = null;
       try {
-        $this->conn = new PDO('mysql:host=' . $CFG->dbhost . ';dbname=' . $CFG->dbname . ';port='. $this->port , $CFG->dbuser, $CFG->dbpass);
+        $this->conn = new PDO('mysql:host=' . $CFG->dbhost . ';dbname=' . $CFG->dbname . ';charset=utf8', $CFG->dbuser, $CFG->dbpass);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
-        echo 'Connection Error: ' . $e->getMessage();
+        try {
+			$this->conn = new PDO('mysql:host=' . $CFG->dbhost . ';dbname=' . $CFG->dbname . ';port='. $this->port . ';charset=utf8', $CFG->dbuser, $CFG->dbpass);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+			echo 'Connection Error: ' . $e->getMessage();
+		}
       }
       return $this->conn;
     }
