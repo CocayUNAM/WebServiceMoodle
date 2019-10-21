d2a00fe9da0acad334ed2475e3e7dd28b8b7d6ff
Juan Carlos Hernández <48781621+JCarlosHA@users.noreply.github.com>
2019-10-21 14:42:03 -0500

    Update Users.php
    Only select valid users

--- WB/models/Users.php
+++ WB/models/Users.php
@@ -11,7 +11,7 @@
     // Get Users
     public function read() {
       // Create query
-      $query = "SELECT DISTINCT us.username, us.firstname, us.lastname, us.email, us.institution, us.city FROM mdl_role_assignments ass INNER JOIN mdl_context con on ass.contextid = con.id INNER JOIN mdl_course cou on con.instanceid = cou.id INNER JOIN mdl_user us on ass.userid = us.id INNER JOIN mdl_user_enrolments ue ON ue.userid = us.id INNER JOIN mdl_assign_grades ag ON ue.userid = ag.userid INNER JOIN mdl_role rl ON rl.id = ass.roleid WHERE rl.shortname='student' AND ag.grade=(select max(grade) from mdl_assign_grades as f where ue.userid = f.userid) ORDER BY us.lastname, us.firstname";
+      $query = "SELECT DISTINCT us.username, us.firstname, us.lastname, us.email, us.institution, us.city FROM mdl_role_assignments ass INNER JOIN mdl_context con on ass.contextid = con.id INNER JOIN mdl_course cou on con.instanceid = cou.id AND cou.idnumber LIKE '%\_%' INNER JOIN mdl_user us on ass.userid = us.id INNER JOIN mdl_user_enrolments ue ON ue.userid = us.id INNER JOIN mdl_assign_grades ag ON ue.userid = ag.userid INNER JOIN mdl_role rl ON rl.id = ass.roleid WHERE rl.shortname='student' AND ag.grade=(select max(grade) from mdl_assign_grades as f where ue.userid = f.userid) ORDER BY us.lastname, us.firstname";
       // Prepare statement
       $stmt = $this->conn->prepare($query);
       // Execute query
